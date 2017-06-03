9f102afc4d1c465cb160d1c6f2761f8acf1ba5ca
Anacelia Gillette <acgillette426@gmail.com>
2017-06-02 20:15:47 -0700

    maybe this will work

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var setHeaderColor = function() {
+  var hue = Math.floor(Math.random() * 360);
+  var pastel = 'hsl(' + hue + ', 100%, 87.5%)';
+  $(".name").css("color", pastel);
+};
+
+$(document).ready(function(){
+  setInterval(setHeaderColor, 1000);
+
+  $(".title").hover(function() {
+    $(this).prev().addClass("fa-spin");
+  }, function() {
+    $(this).prev().removeClass("fa-spin");
+  });
+
+  $("a").hover(function() {
+    var hue = Math.floor(Math.random() * 360);
+    var pastel = 'hsl(' + hue + ', 100%, 70%)';
+    $(this).css("color", pastel);
+
+  }, function() {
+    $(this).removeAttr('style');
+  });
+
+});
