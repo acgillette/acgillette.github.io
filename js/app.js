ab25d0273813338ac25e5f927e8cbf3b15ba7745
Anacelia Gillette <acgillette426@gmail.com>
2017-06-02 20:04:02 -0700

    Added some sick JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-$(document).foundation()
+$(document).foundation();
+
+var setHeaderColor = function() {
+  var hue = Math.floor(Math.random() * 360);
+  var pastel = 'hsl(' + hue + ', 100%, 87.5%)';
+  $(".name").css("color", pastel);
+};
+
+$(document).ready(function(){
+  setInterval(setHeaderColor, 1000);
+
+  $(".title").hover(function() {
+    $(this).prev().addClass("fa-spin");
+  }, function() {
+    $(this).prev().removeClass("fa-spin");
+  });
+
+  $("a").hover(function() {
+    var hue = Math.floor(Math.random() * 360);
+    var pastel = 'hsl(' + hue + ', 100%, 70%)';
+    $(this).css("color", pastel);
+
+  }, function() {
+    $(this).removeAttr('style');
+  });
+
+});
