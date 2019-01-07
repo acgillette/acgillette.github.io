bfee0e656801feb53e205f8b705b3978c1844ef3
acgillette <acgillette426@gmail.com>
2019-01-07 17:04:12 -0500

    Updating bio and links

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 :dizzy: My personal resume/portfolio site :dizzy:
 
 ## Site To Do
-* Add goodreads API to add my currently reading 
 * Add back in blogs page redesigned
 * Shifting background gradients
+* Add more CSS themes to choose from
 * Add commits section showing latest commits
 
 
--- webpack/entry.js
+++ webpack/entry.js
@@ -8,36 +8,36 @@ $(document).ready(() => {
         var hue = Math.floor(Math.random() * 360);
         var pastel = 'hsl(' + hue + ', 100%, 70%)';
         $(this).css({"color": pastel});
-        console.log(this);
     }, function() {
         $(this).removeAttr('style');
     });
 
-    const proxyUrl = proxify(
-        `https://www.goodreads.com/review/list.xml?key=${GOODREADS_API_KEY}&id=${GOODREADS_USER_ID}&shelf=currently-reading`,
-        { inputFormat: 'xml' }
-    );
+    // To investigate: why proxify is suddenly failing
+    // const proxyUrl = proxify(
+    //     `https://www.goodreads.com/review/list.xml?key=${GOODREADS_API_KEY}&id=${GOODREADS_USER_ID}&shelf=currently-reading`,
+    //     { inputFormat: 'xml' }
+    // );
         
-    fetch(proxyUrl)
-    .then((result) => {
-        return result.json();
-    })
-    .then((json) => {
-        const books = json.query.results.GoodreadsResponse.books.book;
-        if (books.length) {
-            books.forEach((book) => {
-                $("<a>", {
-                    text: book.title,
-                    href: book.link,
-                    style: "padding-right: 1%;"
-                }).appendTo(".books");
-            });    
-        } else {
-            $("<a>", {
-                text: books.title,
-                href: books.link,
-                style: "padding-right: 1%;"
-            }).appendTo(".books");
-        }
-    })
+    // fetch(proxyUrl)
+    // .then((result) => {
+    //     return result.json();
+    // })
+    // .then((json) => {
+    //     const books = json.query.results.GoodreadsResponse.books.book;
+    //     if (books.length) {
+    //         books.forEach((book) => {
+    //             $("<a>", {
+    //                 text: book.title,
+    //                 href: book.link,
+    //                 style: "padding-right: 1%;"
+    //             }).appendTo(".books");
+    //         });    
+    //     } else {
+    //         $("<a>", {
+    //             text: books.title,
+    //             href: books.link,
+    //             style: "padding-right: 1%;"
+    //         }).appendTo(".books");
+    //     }
+    // })
 });
